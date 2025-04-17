c414054dfbd35f95933a29d4e543da80ef406b4b
song_eun <dmswo546@gmail.com>
2025-04-17 10:05:54 +0900

    style: 스타일 수정

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -13,11 +13,10 @@ const ProductCard = ({ data }) => {
         ) : (
           <span></span>
         )}
-        {/* <span className={css.discount}>{`${data.discount}%`}</span> */}
       </div>
       <div className={css.textWrap}>
         <strong className={css.title}>{data.title}</strong>
-        <span className={css.price}>₩{Number(data.price).toLocaleString()}</span>
+        <span className={css.price}>{`₩ ${Number(data.price).toLocaleString()}`}</span>
       </div>
       <Link to={`/detail/${data.id}`} className={css.btnGoDetail}>
         상품 상세 페이지
