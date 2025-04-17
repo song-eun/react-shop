aa5ce8c80e1c3f245046e9ae74353273dfed0f65
song_eun <dmswo546@gmail.com>
2025-04-17 16:36:58 +0900

    style: 디테일 페이지 스타일 수정

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -12,11 +12,29 @@ const DetailPage = () => {
   const renderContent = () => {
     switch (activeTab) {
       case 'desc':
-        return <p>상품 설명입니다.</p>
-      case 'detail':
-        return <p>상세 설명입니다.</p>
+        return (
+          <p>
+            상품 설명입니다. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Laboriosam dolorem ea numquam maxime, at repellendus nostrum enim culpa quos velit
+            maiores omnis? Rem mollitia quam vel, voluptate et adipisci eum.
+          </p>
+        )
+      case 'info':
+        return (
+          <p>
+            상세 설명입니다. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Laboriosam dolorem ea numquam maxime, at repellendus nostrum enim culpa quos velit
+            maiores omnis? Rem mollitia quam vel, voluptate et adipisci eum.
+          </p>
+        )
       case 'review':
-        return <p>리뷰 내용입니다.</p>
+        return (
+          <p>
+            리뷰 내용입니다. <br /> Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Laboriosam dolorem ea numquam maxime, at repellendus nostrum enim culpa quos velit
+            maiores omnis? Rem mollitia quam vel, voluptate et adipisci eum.
+          </p>
+        )
     }
   }
 
@@ -41,36 +59,30 @@ const DetailPage = () => {
           </div>
         </div>
       </div>
-      <div>
-        <ul className="nav nav-underline">
-          <li className="nav-item">
-            <button
-              className={`nav-link ${activeTab === 'desc' ? 'active' : ''}`}
-              onClick={() => setActiveTab('desc')}
-            >
-              Description
-            </button>
-          </li>
-          <li className="nav-item">
-            <button
-              className={`nav-link ${activeTab === 'detail' ? 'active' : ''}`}
-              onClick={() => setActiveTab('detail')}
-            >
-              Additional information
-            </button>
-          </li>
-          <li className="nav-item">
-            <button
-              className={`nav-link ${activeTab === 'review' ? 'active' : ''}`}
-              onClick={() => setActiveTab('review')}
-            >
-              Reviews(0)
-            </button>
-          </li>
-        </ul>
-        <div className="tab-content mt-3">{renderContent()}</div>
-      </div>
-      <div>
+      <section className={css.tab}>
+        <div className={css.tabBtns}>
+          <button
+            className={` ${activeTab === 'desc' ? css.active : ''}`}
+            onClick={() => setActiveTab('desc')}
+          >
+            Description
+          </button>
+          <button
+            className={` ${activeTab === 'info' ? css.active : ''}`}
+            onClick={() => setActiveTab('info')}
+          >
+            Additional information
+          </button>
+          <button
+            className={` ${activeTab === 'review' ? css.active : ''}`}
+            onClick={() => setActiveTab('review')}
+          >
+            Reviews(0)
+          </button>
+        </div>
+        <div className={css.tabContent}>{renderContent()}</div>
+      </section>
+      <section className={css.relatedItems}>
         <h2>Similar Items</h2>
         <Swiper
           slidesPerView={3}
@@ -86,7 +98,7 @@ const DetailPage = () => {
             </SwiperSlide>
           ))}
         </Swiper>
-      </div>
+      </section>
     </main>
   )
 }
